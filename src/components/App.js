18d6b615dff75493df734172f0fc0fc46dc982c2
Suji-12 <23csec23sujithas@gmail.com>
2026-01-04 11:59:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,14 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [count, setCount] = useState(0);
   return (
     <div>
-        {/* Do not remove the main div */}
+        {/* Do not remove the main div */
+          <button onClick={() => setCount(count + 1)}>
+          Click me
+          </button>
+        }
     </div>
   )
 }
